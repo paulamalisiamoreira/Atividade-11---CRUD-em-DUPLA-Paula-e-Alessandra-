39612b789318aab811a78cf19f4323655e5d6b90
AlessandraCruz6 <alessandra_c_cruz@estudante.sesisenai.org.br>
2024-09-30 08:59:12 -0300

    pagina connection

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "notas";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+}
