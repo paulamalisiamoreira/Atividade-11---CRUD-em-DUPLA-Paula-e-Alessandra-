a39a3e321a62c7e3e8efc1ef60c1ce6cc4c59f92
alessandra_c_cruz@estudante.sesisenai.org.br <AlessandraCruz6>
2024-10-01 08:10:37 -0300

    pagina

--- index.php
+++ index.php
@@ -1,38 +1,7 @@
 <?php
 include "./connection.php";
-if (isset($_POST['create'])) {
-    $titulo= $_POST['titulo'];
-    $conteudo = $_POST['conteudo'];
-    
 
-    $sql = "INSERT INTO aulas(titulo, conteudo) VALUES ('$titulo', '$conteudo')";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "Adicionado com sucesso";
-    } else {
-        echo "Erro: " . $sql . "<br>" . $conn->error;
-    }
-}
-
-?>
-
-
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página Inicial</title>
-</head>
-<body>
-
-  <input type="submit" name="create" value="adicionar nota">
-   
-    
 
 
 
     
-</body>
-</html>
-<?php $conn->close() ?>
